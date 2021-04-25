062bfee4b4283b92fae7278aac797531dc35cc14
amir <amirhaghparast412@gmail.com>
2021-04-25 13:58:53 +0430

    last commit

--- blog/models.py
+++ blog/models.py
@@ -1,7 +1,8 @@
 from django.db import models
 from django.utils import timezone
 from django.contrib.auth.models import User
-from django.utils.translation import gettext as _  
+from django.utils.translation import gettext as _ 
+
 #Create your models here.
 class blog(models.Model):
     title = models.CharField(_("عنوان"),max_length=50)
--- restaurant/settings.py
+++ restaurant/settings.py
@@ -42,6 +42,7 @@ INSTALLED_APPS = [
     'reservation.apps.ReservationConfig',
     'blog.apps.BlogConfig',
     'contact',
+    
 ]
 
 MIDDLEWARE = [
